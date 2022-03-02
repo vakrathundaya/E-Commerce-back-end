652dbccd990a8647842fb203836e0de543ca92e8
Radhika <cu9rad@gmail.com>
2022-03-02 18:50:58 -0500

    category get routes addded

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,34 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with that id" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+
+  }
 });
 
 router.post('/', (req, res) => {
