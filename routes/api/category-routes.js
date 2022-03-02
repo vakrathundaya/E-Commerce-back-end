23090a7398e893cb1d2934203ed14db25a507bd6
Radhika <cu9rad@gmail.com>
2022-03-02 18:58:51 -0500

    post and put routes functionality added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,10 +38,31 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+
+  }
 });
 
 router.delete('/:id', (req, res) => {
