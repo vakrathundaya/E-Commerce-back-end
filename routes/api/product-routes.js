a312e76c260553de08f999c850f3c66ac680a0c7
Radhika <cu9rad@gmail.com>
2022-03-02 19:36:11 -0500

    get routes added for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }]
+
+    })
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.fingByPk(req.params.id, {
+      includ: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }]
+    })
+    if (!productData) {
+      res.status(404).json({ message: "No data found with taht id" });
+      return;
+    }
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json
+  }
 });
 
 // create new product
