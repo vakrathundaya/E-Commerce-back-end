1062f8c2c3d85126f01fd3539b9693895c22c547
Radhika <cu9rad@gmail.com>
2022-03-04 19:43:51 -0500

    delete tag functionality added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,7 +55,7 @@ const tagData = await Tag.update(req.body,{
   }
 })
 if(!tagData){
-  res.status(404).json({message: " no Tage found"});
+  res.status(404).json({message: "No Tag found"});
   return;
 }
 res.status(200).json(tagData);
@@ -65,8 +65,24 @@ res.status(200).json(tagData);
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if(!tagData){
+      res.status(404).json({message : "No tag Data Found"})
+    return;
+    }
+    res.status(200).json(tagData);
+  }
+catch(err){
+res.status(500).json(err);
+}
+
 });
 
 module.exports = router;
